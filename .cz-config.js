6b05f8367b0721a2f1f02ac884f135ff5c2dd210
曾宪华 <huahua_lanh@163.com>
2021-01-08 16:04:41 +0800

    feat(accounts): feat(accounts): 特性(模块1): 这是可以的?

--- .cz-config.js
+++ .cz-config.js
@@ -4,15 +4,15 @@ module.exports = {
 
   types: [
     {value: 'feat',     name: '特性:    一个新的特性'},
-    {value: 'fix',      name: 'fix:      A bug fix'},
-    {value: 'docs',     name: 'docs:     Documentation only changes'},
-    {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
-    {value: 'perf',     name: 'perf:     A code change that improves performance'},
-    {value: 'test',     name: 'test:     Adding missing tests'},
-    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
-    {value: 'revert',   name: 'revert:   Revert to a commit'},
-    {value: 'WIP',      name: 'WIP:      Work in progress'}
+    {value: 'fix',      name: '修复:    修复一个Bug'},
+    {value: 'docs',     name: '文档:    变更的只有文档'},
+    {value: 'style',    name: '格式:    空格, 分号等格式修复'},
+    {value: 'refactor', name: '重构:    代码重构，注意和特性、修复区分开'},
+    {value: 'perf',     name: '性能:    提升性能'},
+    {value: 'test',     name: '测试:    添加一个测试'},
+    {value: 'chore',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
+    {value: 'revert',   name: '回滚:    代码回退'},
+    // {value: 'WIP',      name: 'WIP:      Work in progress'}
   ],
 
   scopes: [
@@ -35,8 +35,8 @@ module.exports = {
   */
   // override the messages, defaults are as follows
   messages: {
-    type: 'Select the type of change that you\'re committing:',
-    scope: '\nDenote the SCOPE of this change (optional):',
+    type: '选择一种你的提交类型:',
+    scope: '选择一个scope (可选):',
     // used if allowCustomScopes is true
     customScope: 'Denote the SCOPE of this change:',
     subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
